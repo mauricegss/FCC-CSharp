609ee470e0ffe2f5546bf27000cdb80c2dffc0b8
Maurice Santos <mauricegma@gmail.com>
2025-09-08 11:33:18 -0300

    part 5
    5. Create Methods in C# Console Applications

---
+++ 5_CreateMethods/Saves/Circle.cs
@@ -0,0 +1,22 @@
+﻿PrintCircleInfo(12);
+
+void PrintCircleArea(int radius)
+{
+    double pi = 3.14159;
+    double area = pi * (radius * radius);
+    Console.WriteLine($"Area = {area}");
+}
+
+void PrintCircleCircumference(int radius)
+{
+    double pi = 3.14159;
+    double circumference = 2 * pi * radius;
+    Console.WriteLine($"Circumference = {circumference}");
+}
+
+void PrintCircleInfo(int radius) 
+{
+    Console.WriteLine($"Circle with radius {radius}");
+    PrintCircleArea(radius);
+    PrintCircleCircumference(radius);
+}
